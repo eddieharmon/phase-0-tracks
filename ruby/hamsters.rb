54676d4e34a998bd304854679616f30af9041a4a
Eddie Harmon <eddieharmon@hotmail.com>
2016-12-11 19:25:05 -0500

    Update adoptable variable

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -9,11 +9,13 @@ hamster_age = gets.chomp
 hamster_age == nil
 
 puts "Is the hamster a good candidate for adoption? (y/n)"
-adoptable = gets.chomp
+adoptable = gets.chomp.to_s
 
 if adoptable == "y"
   puts "That's great news!"
+  adoptable = "more than ready"
 elsif adoptable == "n"
+  adoptable = "unable"
   puts "Awwww, so sad to hear..."
 else
   puts "I don't know what you mean."
@@ -21,9 +23,7 @@ end
 #y = "more than ready"
 #n = "unable"
 
-#puts "What is the hamster's volume level? (1-10)"
-#hamster_volume = gets.chomp => 1 =<10
+puts "What is the hamster's volume level? (1-10)"
+hamster_volume = gets.chomp.to_i
 
-
-
-puts "Your most recently registered hamster is a #{hamster_color} hamster named #{hamster_name}. #{hamster_name} is #{hamster_age} years old. #{hamster_name} is #{adoptable} to be adopted at this time."
+puts "Your most recently registered hamster is a #{hamster_color} hamster named #{hamster_name}. #{hamster_name} is #{hamster_age} years old and has a noise rating of #{hamster_volume}. #{hamster_name} is #{adoptable} to be adopted at this time. "
