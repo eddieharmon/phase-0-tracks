e5b0f771ca6a4d93d6f2a50f24d08cf2996a7eb8
Eddie Harmon <eddieharmon@hotmail.com>
2016-12-19 05:28:22 -0500

    Problem solving methods

---
+++ ruby/problem_solving_methods.rb
@@ -0,0 +1,26 @@
+#multiple = [5,10,15,20,25]
+
+#p find (multiple, 2)
+
+def search_array(array, numbers)
+
+  index= 0
+  answer = nil;
+
+  while index < array.length && answer==nil
+    if array[index] == numbers
+      #return index
+      answer = index
+    else
+      #return nil
+      answer = nil
+    end
+    index +=1
+  end
+answer
+end
+
+arr = [42,89,23,1]
+
+p search_array(arr,1)
+p search_array(arr,24)
