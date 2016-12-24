12b078f45c88979e5d9be191aa0045ecb6d2887d
Eddie Harmon <eddieharmon@hotmail.com>
2016-12-24 11:45:28 -0500

    create puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+# SPECIES ------------------------
+# Puppies
+
+# CHARACTERISTICS ----------------
+# Furry
+# Friendly: yes
+# Eye count: 2
+# Name: varies
+# Color: varies
+
+# BEHAVIOR -----------------------
+# Barks
+# Wags Tail
+# Fetches
