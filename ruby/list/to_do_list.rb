6393dbfadbfa0f0664051ae1f293cea7795d8eef
Eddie Harmon <eddieharmon@hotmail.com>
2017-01-03 02:53:52 -0500

    Tyler I know you want me to commit more and I will try.

---
+++ ruby/list/to_do_list.rb
@@ -0,0 +1,50 @@
+class TodoList
+    def initialize(list_items)
+        @list = list_items
+    end
+
+    def get_items
+        @list
+    end
+
+    def add_item(list_items)
+        @list << list_items
+    end
+
+    def delete_item(list_items)
+        @list.delete(list_items)
+    end
+
+    def get_item(list_items)
+        @list[list_items]
+    end
+
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class TodoList
+# def initialize(list_items).split
+#   @list = list_items
+# end
+
+
+
+
+# end
+
+
+# list_items.("do the dishes", "mow the lawn")
