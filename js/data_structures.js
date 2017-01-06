b2fe6d6fffae0174030c7bff20002f56cb9e1ccf
Eddie Harmon <eddieharmon@hotmail.com>
2017-01-06 00:44:19 -0500

    add driver code for instance variables and add new function that prints honk sound. Demonstrate how to use this to produce any instance variables using the same method.

--- js/data_structures.js
+++ js/data_structures.js
@@ -13,11 +13,19 @@ console.log(horses)
 
 
 function Cars(make, model, year) {
-  console.log("Our new car:", this);
   this.make = make;
   this.model = model;
   this.year = year;
+  if (this.year <2013){
+    console.log("You have an old car.");
+  }
+  else {console.log("You have a new car.");}
 }
 
 var anotherCar = new Cars("Honda", "Civic", 2014);
 console.log("You have a " + anotherCar.model + " made by " +anotherCar.make + "in " + anotherCar.year);
+var otherCar = new Cars("Toyota", "Prius", 2000);
+console.log("You have a " + otherCar.model + " made by " +otherCar.make + "in " + otherCar.year);
+
+otherCar.honk = function(){console.log("Honk Honk!")};
+otherCar.honk();
