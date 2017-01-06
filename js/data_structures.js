9d126626bc127a7ef9f4ac8208c4065055a9094f
Eddie Harmon <eddieharmon@hotmail.com>
2017-01-06 00:03:30 -0500

    create object using 2 arrays. Names array is a key and colors array is a value in the object.

--- js/data_structures.js
+++ js/data_structures.js
@@ -4,5 +4,9 @@ var names = ["James", "Seabiscuit", "Flicka", "Shadowfox"];
 colors.push("black");
 names.push("Felitz");
 
+var horses = {};
+for (i = 0; i < names.length; i++){
+  horses[names[i]]=colors[i];
+}
 
-
+console.log(horses)
