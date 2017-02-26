849835673591fa3c286afca96ed54454d0f616dd
Eddie Harmon <eddieharmon@hotmail.com>
2017-02-26 15:11:52 -0500

    Added specific y/n to insurance question to clarify user input

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -15,7 +15,7 @@ age = gets.chomp
 puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
 garlic_bread = gets.chomp
 
-puts "Would you like to enroll in the company’s health insurance?"
+puts "Would you like to enroll in the company’s health insurance (y/n)?"
 insurance = gets.chomp
 
 if age && garlic_bread && insurance == true
