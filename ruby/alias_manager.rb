125e19a49f93b38417cef01b2193fb9e4b73f1cf
Eddie Harmon <eddieharmon@hotmail.com>
2017-03-17 18:54:14 -0400

    Added method for alias manager along with pseudocode.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,11 +1,18 @@
-codename = "Felicia Torres"
-codename = codename.split(' ').reverse!
+# pseudocode and write a method that takes a spy's real name (e.g., "Felicia Torres") and creates a fake name with it by doing the following:
 
+# 1) Swapping the first and last name.
 
-#codename.tr('aeiou', 'bfjpv')
-#codename.chars
-# I tried to perform a chain method on my alias but it keeps breaking after the third method is added. My line 5 code does work, but I am trying to figure out how to store the change I have made to the string, into a new string so that I can make an additional change on it. I believe .map does this but something must be wrong with the way I am applying it. Should I use the ! operator?
+# Tried to use .shuffle since it was only two names, but it is unreliable.
+# .rotate! works even without the parameter (since there are only two values, but will fail if there was a middle name).
+# Use .join with a space parameter to seperate the names after being joined.
 
-# puts "Enter your first and last name."
-#name = gets.chomp
-#gets.chomp >> fake_name_generator
+def alias_creator(name)
+  puts "Your name backwards is #{name.split(' ').rotate!.join(' ')}."
+end
+
+#DRIVER CODE
+alias_creator("Felicia Torres")
+
+# 2) Changing all of the vowels (a, e, i, o, or u) to the next vowel in 'aeiou', and all of the consonants (everything else besides the vowels) to the next consonant in the alphabet. So 'a' would become 'e', 'u' would become 'a', and 'd' would become 'f'.
+
+# This sounds like I will need to iterate through each letter, and then use some sort of method to determine if it meets a certain criteria like we did in a previous assignment. This sounds like a code block will be needed.
