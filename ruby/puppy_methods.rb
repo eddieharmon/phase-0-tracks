e4d4044e9cdd7e62a427900a9855ce63a0583ac4
Eddie Harmon <eddieharmon@hotmail.com>
2017-03-21 21:28:21 -0400

    Added pseudocode and iterated through the stored_instances array to run the method calls.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,62 +1,62 @@
-class Puppy
+# class Puppy
 
-  def fetch(toy)
-    puts "I have just fetched a #{toy}!"
-  end
+#   def fetch(toy)
+#     puts "I have just fetched a #{toy}!"
+#   end
 
-  def speak(x)
-    x.times { puts "Woof!"}
-  end
+#   def speak(x)
+#     x.times { puts "Woof!"}
+#   end
 
-  def roll_over
-    puts "*rolls over*"
-  end
+#   def roll_over
+#     puts "*rolls over*"
+#   end
 
-  def dog_years(x)
-    puts x * 7
-  end
+#   def dog_years(x)
+#     puts x * 7
+#   end
 
-  def play_dead
-    puts "*plays dead*"
-  end
+#   def play_dead
+#     puts "*plays dead*"
+#   end
 
-  def initialeze
-    puts "Initializing new puppy instance..."
-  end
+#   def initialeze
+#     puts "Initializing new puppy instance..."
+#   end
 
-end
+# end
 
-Puppy.new
-
-
-duchess = Puppy.new
-fido = Puppy.new
-spot = Puppy.new
+# Puppy.new
 
 
-# Driver Code
-
-spot.fetch("ball")
-fido.fetch("stick")
-duchess.fetch("frisbee")
-
-spot.speak(3)
-fido.speak(2)
-duchess.speak(5)
-
-spot.roll_over
-fido.roll_over
-duchess.roll_over
-
-# How can I distinguish which instance of my method call is being run?
-spot.dog_years(3)
-fido.dog_years(2)
-duchess.dog_years(5)
+# duchess = Puppy.new
+# fido = Puppy.new
+# spot = Puppy.new
 
 
-spot.play_dead
-fido.play_dead
-duchess.play_dead
+# # Driver Code
+
+# spot.fetch("ball")
+# fido.fetch("stick")
+# duchess.fetch("frisbee")
+
+# spot.speak(3)
+# fido.speak(2)
+# duchess.speak(5)
+
+# spot.roll_over
+# fido.roll_over
+# duchess.roll_over
+
+# # How can I distinguish which instance of my method call is being run?
+# spot.dog_years(3)
+# fido.dog_years(2)
+# duchess.dog_years(5)
+
+
+# spot.play_dead
+# fido.play_dead
+# duchess.play_dead
 
 
 # Release 2: Implement my own class below the Puppy class
@@ -67,22 +67,63 @@ def initialize
   puts "Initializing new Apple instance..."
 end
 
-def iphone
+def phone_call
+  puts "Who would you like to speak to?"
 end
 
-def ipod
+def music
+  puts "Press play on your favorite album."
 end
 
-def imac
+def surf
+  puts "Open up the internet to browse the web."
 end
 
 
 end
 
+Apple.new
 
+iphone = Apple.new
+ipod = Apple.new
+imac = Apple.new
 
+# Driver code for Apple class
 
+# loop do 50.times
+#   class Apple
+#   end
+# end
+# 50.times {Apple.new}
+# instance_catcher = 50.times {Apple.new}
+# Modify your loop so that it stores all of the instances in a data structure.
 
+stored_instances = []
+#stored_instances << instance_catcher # I had something to "catch" the 50 new instances, but it isn't needed. Line 104
+50.times do
+stored_instances << Apple.new
+end
+puts "stored_instances"
+p stored_instances
+
+#method_calls = iphone.phone_call, ipod.music, imac.surf
+
+stored_instances.each do |item| #Don't call the original method calls, must use what's in the pipes then the method
+item.phone_call
+item.music
+item.surf
+end
+
+# Method Calls
+# iphone.phone_call
+# ipod.music
+# imac.surf
+
+# This does not work...
+# def make_more_instances
+#   50.times class Apple
+#   end
+# end
 
 
 
