5feb9ce3727e656693656b78bbb294a2d3158f54
Eddie Harmon <eddieharmon@hotmail.com>
2017-03-26 02:15:09 -0400

    Added driver code to new Shout module mixin.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -14,7 +14,13 @@ class Protesters
 end
 
 
+#DRIVER CODE
 
+fans = Fans.new
+fans.scream("cheaper!")
+
+protesters = Protesters.new
+protesters.scream("FREE!")
 
 
 
@@ -36,7 +42,6 @@ end
 
 
 
-#-------------------------------------------------------------------
 
 
 
@@ -54,39 +59,3 @@ end
 
 
 
-# module Shout
-#   def self.yell_angrily(words)
-#     words + "!!!" + " :("
-#   end
-
-#   def self.yelling_happily(screams)
-#     screams + "yayyyyy!!!" + " :)"
-#   end
-
-# end
-
-# #Driver Code
-
-# Shout.yell_angrily("I'm such an idiot!")
-# Shout.yelling_happily("YeahBoyyyyy")
-
-
-#  Parents
-#   include Shout
-# end
-
-# class Chubby_Checker
-#   include Shout
-# end
-
-# parents = Parents.new
-# parents.yell_angrily("Clean up your room!")
-
-# chubby_checker = Chubby_Checker.new
-# chubby_checker.yelling_happily("Twist and Shout!")
-# class
-
-# why do I keep getting these messages?
-# shout.rb:27:in `<main>': undefined method `yell_angrily' for #<Parents:0x007fad8a892670> (NoMethodError)
-
-# shout.rb:15:in `<main>': undefined method `yelling_happily' for Shout:Module (NoMethodError)
