285682eb8829412002e49d58d54222dcb28729c4
Eddie Harmon <eddieharmon@hotmail.com>
2017-03-26 01:57:18 -0400

    Completed standalone module.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,34 +1,77 @@
-module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
-
-  def self.yelling_happily(screams)
-    screams + "yayyyyy!!!" + " :)"
-  end
-
-end
-
-#Driver Code
-
-Shout.yell_angrily("I'm such an idiot!")
-Shout.yelling_happily("YeahBoyyyyy")
 
 
- Parents
-  include Shout
-end
 
-class Chubby_Checker
-  include Shout
-end
 
-parents = Parents.new
-parents.yell_angrily("Clean up your room!")
 
-chubby_checker = Chubby_Checker.new
-chubby_checker.yelling_happily("Twist and Shout!")
-class
+
+
+# Standalone version below
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yelling_happily(words)
+#     words + "!!!!!!" + " :)"
+#   end
+
+# end
+
+# p Shout.yell_angrily("&*^*%&^*&^ARGGHHHHH&&^^&^")
+
+# p Shout.yelling_happily("YESSSSSSSSSSSS")
+
+
+
+#-------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yelling_happily(screams)
+#     screams + "yayyyyy!!!" + " :)"
+#   end
+
+# end
+
+# #Driver Code
+
+# Shout.yell_angrily("I'm such an idiot!")
+# Shout.yelling_happily("YeahBoyyyyy")
+
+
+#  Parents
+#   include Shout
+# end
+
+# class Chubby_Checker
+#   include Shout
+# end
+
+# parents = Parents.new
+# parents.yell_angrily("Clean up your room!")
+
+# chubby_checker = Chubby_Checker.new
+# chubby_checker.yelling_happily("Twist and Shout!")
+# class
 
 # why do I keep getting these messages?
 # shout.rb:27:in `<main>': undefined method `yell_angrily' for #<Parents:0x007fad8a892670> (NoMethodError)
