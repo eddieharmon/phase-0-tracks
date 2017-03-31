702c4a9c7e8658f8f12151199fda2aa57f816f3a
Eddie Harmon <eddieharmon@hotmail.com>
2017-03-31 16:04:37 -0400

    add car constructor.

--- js/data_structures.js
+++ js/data_structures.js
@@ -19,3 +19,28 @@ console.log(horses);
 
 //Car Constructor
 
+function Car (make, model, year, used) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.used = used;
+  this.honk = function(){console.log("HONK! HONK!");};
+
+}
+
+var firstCar = new Car("Toyota", "Camry", 1994, true);
+console.log(firstCar);
+firstCar.honk();
+
+var secondCar = new Car("BMW", "X5", 2001, true);
+console.log(secondCar);
+secondCar.honk();
+
+var thirdCar = new Car("Honda", "Accord", 2017, false);
+console.log(thirdCar);
+thirdCar.honk();
+
+//Literal Constructor
+
+var car = {make: "Trabant", model: "OstKruez" , year: 1975, used: true};
+console.log(car);
