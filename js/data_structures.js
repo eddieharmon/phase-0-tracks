13e92ce2889c6857dad722a5cf29b5cac14c7767
Eddie Harmon <eddieharmon@hotmail.com>
2017-04-02 21:33:41 -0400

    re-practiced creating the horse object.

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,46 +1,49 @@
-var horseColors = ["Black", "White", "Brown", "Orange"];
+var colors = ["Black", "Brown", "Grey", "Chestnut"];
+var names = ["Mr. Ed", "Seabiscuit", "Secretariat", "Shadowfax"];
 
-var horseNames = ["Fred", "Jenny", "Bob", "Seabiscuit"];
 
-horseColors.push("Grey");
-horseNames.push("Mr. Ed");
+colors.push("Cremello");
+names.push("War Admiral");
 
-console.log(horseColors);
-console.log(horseNames);
+console.log(colors);
+console.log(names);
 
 horses = {};
 
-for (var i = 0; i < horseNames.length; i++) {
-  horses[horseNames[i]] = horseColors[i];
+for(var i = 0; i <= names.length; i++) {
+  horses[names[i]] = colors[i];
 }
 
 console.log(horses);
 
 
-//Car Constructor
 
-function Car (make, model, year, used) {
-  this.make = make;
-  this.model = model;
-  this.year = year;
-  this.used = used;
-  this.honk = function(){console.log("HONK! HONK!");};
 
-}
 
-var firstCar = new Car("Toyota", "Camry", 1994, true);
-console.log(firstCar);
-firstCar.honk();
+// //Car Constructor
 
-var secondCar = new Car("BMW", "X5", 2001, true);
-console.log(secondCar);
-secondCar.honk();
+// function Car (make, model, year, used) {
+//   this.make = make;
+//   this.model = model;
+//   this.year = year;
+//   this.used = used;
+//   this.honk = function(){console.log("HONK! HONK!");};
 
-var thirdCar = new Car("Honda", "Accord", 2017, false);
-console.log(thirdCar);
-thirdCar.honk();
+// }
 
-//Literal Constructor
+// var firstCar = new Car("Toyota", "Camry", 1994, true);
+// console.log(firstCar);
+// firstCar.honk();
 
-var car = {make: "Trabant", model: "OstKruez" , year: 1975, used: true};
-console.log(car);
+// var secondCar = new Car("BMW", "X5", 2001, true);
+// console.log(secondCar);
+// secondCar.honk();
+
+// var thirdCar = new Car("Honda", "Accord", 2017, false);
+// console.log(thirdCar);
+// thirdCar.honk();
+
+// //Literal Constructor
+
+// var car = {make: "Trabant", model: "OstKruez" , year: 1975, used: true};
+// console.log(car);
