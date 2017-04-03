9d630965e3db6f73cbb08fa0b67b966e68fec9b8
Eddie Harmon <eddieharmon@hotmail.com>
2017-04-03 12:47:20 -0400

    add more pseudocode.

--- js/algos.js
+++ js/algos.js
@@ -7,7 +7,7 @@ array = ["dictionary", "ambidextrious", "anotherlongword"]
 for (var i = 0; i <= array.length; i++) {
   console.log(array[i].length);
 }
-
+// The above is counting the length of each item in the array successfully but it is still giving me a "TypeError". Now, the issue is choosing only the highest number. I am aware of .max, but I do not think I can use it based on the instructions of the assignment. I am thinking an if/else statement would be useful in narroring down the highest number, but that approach seems to have its own shortcomings as well.
 
 
 // longest(["dictionary", "ambidextrious", "anotherlongword"]);
