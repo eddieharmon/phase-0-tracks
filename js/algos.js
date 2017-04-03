39e95ba9dd984b44de3a136af6d45108836abf23
Eddie Harmon <eddieharmon@hotmail.com>
2017-04-03 03:39:30 -0400

    add test for loop through array to result in length count of each index.

--- js/algos.js
+++ js/algos.js
@@ -1,1 +1,24 @@
-function [dictionary, ambidextrious, anotherlongword]
+// function longest() {
+
+// }
+
+array = ["dictionary", "ambidextrious", "anotherlongword"]
+
+for (var i = 0; i <= array.length; i++) {
+  console.log(array[i].length);
+}
+
+
+
+// longest(["dictionary", "ambidextrious", "anotherlongword"]);
+
+// The code below shows .length works as expected:
+// array = ["dictionary", "ambidextrious", "anotherlongword"]
+// console.log(array.length);
+
+// console.log("sweet".length);
+
+// console.log(["dictionary", "ambidextrious", "anotherlongword"].length);
+
+
+//I think the program should first iterate through the array and then do a .length on each of the items in the array.
