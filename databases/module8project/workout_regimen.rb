1112e87929630729a280a769ea7a50e3df4eb8c5
Eddie Harmon <eddieharmon@hotmail.com>
2017-04-10 02:16:04 -0400

    add athlete table.

--- databases/module8project/workout_regimen.rb
+++ databases/module8project/workout_regimen.rb
@@ -6,6 +6,22 @@
 require 'sqlite3'
 require 'faker'
 
-
 #Create sqlite3 database
 db = SQLite3::Database.new("fitness.db")
+
+# use a string delimeter
+create_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS athlete(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255)
+  )
+SQL
+
+create_table_cmd_excercise = <<-SQL
+  CREATE TABLE IF NOT EXISTS exercise(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255)
+  )
+SQL
+
+db.execute(create_table_cmd)
